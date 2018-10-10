3f0707a31b2e161f3d22663f859c2d84f13cf663
Hugo Gabriel Bezerra da Silva 114110448 <hugogbs@lcc3-056.lcc.ufcg.edu.br>
2018-10-10 14:18:21 -0300

    Cria método para a inseção dos comentários de um post

--- dev-web-frontend/src/post/Post.js
+++ dev-web-frontend/src/post/Post.js
@@ -3,13 +3,23 @@ import "./Post.css"
 import Comment from '../comment/Comment';
 
 class Post extends Component {
+
+    createCommentsList = () => {
+        let commentsList = [];
+        for (let i = 0; i < 5; i++) {
+            commentsList.push(<div><Comment /></div>)
+        }
+
+        return commentsList;
+
+    }
   render() {
     return (
       <div className="Post">
         Post bem bacana
         <p>Um post pode ter comentários, esse tem:</p>
         <div>
-            <Comment />
+            {this.createCommentsList()}
         </div>
       </div>
     )
